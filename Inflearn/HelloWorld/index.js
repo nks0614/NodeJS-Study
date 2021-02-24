876d210891cb4c2048719f511c0fde8baaa6b5dd
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2021-02-24 21:13:33 +0900

    [UPDATE] Inflearn Node HelloWorld

--- Inflearn/HelloWorld/index.js
+++ Inflearn/HelloWorld/index.js
@@ -1,12 +1,25 @@
-const http = require('http');
+const http = require('http')
 
-const hostname = '127.0.0.1'; // localhost
-const port = 3000; // port
+const hostname = '127.0.0.1' // localhost
+const port = 3000 // port
 
 const server = http.createServer((req, res) => { //http모듈의 createServer메소드
-  res.statusCode = 200; //정상 작동
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  
+  if(req.url === '/'){
+    res.statusCode = 200 //정상 작동
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello, World!\n')
+  } 
+  else if(req.url === '/users'){
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plaun')
+    res.end('User list')
+  }
+  else{
+    res.statusCode = 404
+    res.end('Not Found')
+  }
+
 });
 
 server.listen(port, hostname, () => { //listen메소드는 서버를 요청 대기상태로 만들어줌. 
